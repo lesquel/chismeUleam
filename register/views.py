041aa7c3a282afb1f26c133e325e8cc862a97f89
lesquel <holahi23holahi23@gmail.com>
2024-09-02 19:40:31 -0500

    Refactorizacion de carpetas de la vista de regiter

--- register/views.py
+++ register/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def RegiterView(request):
-    return render(request, 'register/register.html')
+    return render(request, 'pages/register/register.html')
