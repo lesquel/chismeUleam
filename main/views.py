788d144586fe004659fe0cdbc9fe6fa9c99f5e94
SteveSant26 <Bryanmenoscal2005@gmail.com>
2024-09-03 20:31:10 -0500

    Clean repository working tree

--- main/views.py
+++ main/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect, HttpResponse
 
 # Create your views here.
 def home(request):
     if not request.user.is_authenticated:
-        return HttpResponse("No tienes permiso para ver esta página.")
+        return redirect('register:login')
     if request.method == 'GET':
         return HttpResponse("Has enviado un formulario.")
