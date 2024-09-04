6a69834f011d5084a5ad8889a2c7efbc815815b4
lesquel <holahi23holahi23@gmail.com>
2024-09-03 20:57:02 -0500

    register/views

--- register/views.py
+++ register/views.py
@@ -6,29 +6,38 @@ from django.contrib.auth.models import User
 
 # Create your views here.
 def signup_view(request):
+    # Si el usuario esta autenticado, redirige a la pagina principal
     if request.user.is_authenticated:
         return redirect('main:home')
+    # Si el usuario no esta autenticado, muestra el formulario de registro
     if request.method == 'POST':
+        # Obtenemos los datos del formulario
         username = request.POST['username']
         password1 = request.POST['password1']
         password2 = request.POST['password2']
         terminos = request.POST.get('terminos')
-        
+
+        # Comprobamos que las contraseñas sean iguales
         if password1 != password2:
             messages.error(request, "Las contraseñas no coinciden.")
             return render(request, 'register/pages/signup.html')
-        elif len(password1) < 8:
+        # Comprobamos que la contraseña tenga al menos 8 caracteres
+        if len(password1) < 8:
             messages.error(request, "La contraseña debe tener al menos 8 caracteres.")
             return render(request, 'register/pages/signup.html')
-        elif User.objects.filter(username=username).exists():
+        # Comprobamos que el nombre de usuario no este en uso
+        if User.objects.filter(username=username).exists():
             messages.error(request, "El nombre de usuario ya está en uso.")
             return render(request, 'register/pages/signup.html')
-        elif terminos != 'on':
+        # Comprobamos que el usuario acepte los términos y condiciones
+        if terminos != 'on':
             messages.error(request, "Debes aceptar los términos y condiciones.")
             return render(request, 'register/pages/signup.html')
-        
+        # Creamos el usuario
         user = User.objects.create_user(username=username, password=password1)
+        # Iniciamos sesión
         login(request, user)
+        # Mostramos un mensaje de exito
         messages.success(request, "Usuario creado exitosamente.")
         return redirect('main:home')
         
