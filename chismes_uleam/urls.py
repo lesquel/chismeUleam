0380b2df50ec5ed5a64dbc5ef4065d4f97922546
SteveSant26 <Bryanmenoscal2005@gmail.com>
2024-09-03 20:12:21 -0500

    agregue la logica para crear cuenta y iniciar sesion, ademas de sus respectivas validaciones

--- chismes_uleam/urls.py
+++ chismes_uleam/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include('register.urls')),
     path("__reload__/", include("django_browser_reload.urls")),
+    path('', include('main.urls', namespace='main')),
+    path('', include('register.urls',namespace='register')),
 ]
