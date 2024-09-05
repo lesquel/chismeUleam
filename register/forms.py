5bbd318ee8dec2c71a0eaae3768a1c15e5664ca9
SteveSant26 <Bryanmenoscal2005@gmail.com>
2024-09-05 16:37:14 -0500

    agregue el formulario de signup y login desde el archivo forms en register

---
+++ register/forms.py
@@ -0,0 +1,74 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+
+
+class SignUpForm(UserCreationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "bg-blanco border-2 border-verder_claro rounded-lg focus:border-verder_claro focus:ring-verder_claro",
+                "placeholder": "Ingrese su usuario",
+            }
+        )
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "bg-blanco border-2 border-verder_claro rounded-lg focus:border-verder_claro focus:ring-verder_claro",
+                "placeholder": "Ingrese su contrasena",
+            }
+        )
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "bg-blanco border-2 border-verder_claro rounded-lg focus:border-verder_claro focus:ring-verder_claro",
+                "placeholder": "Confirme su contrasena",
+            }
+        )
+    )
+    checkbox = forms.BooleanField(
+        widget=forms.CheckboxInput(
+            attrs={
+                "class": "bg-blanco border-2 border-verder_claro focus:border-verder_claro focus:ring-verder_claro",
+            }
+        )
+    )
+
+    class Meta:
+        model = User
+        fields = ("username", "password1", "password2", "checkbox")
+
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "bg-blanco border-2 border-verder_claro rounded-lg focus:border-verder_claro focus:ring-verder_claro",
+                "placeholder": "Ingrese su usuario",
+            }
+        )
+    )
+
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "bg-blanco border-2 border-verder_claro rounded-lg focus:border-verder_claro focus:ring-verder_claro",
+                "placeholder": "Ingrese su contrasena",
+            }
+        )
+    )
+    checkbox = forms.BooleanField(
+        required=False,
+        widget=forms.CheckboxInput(
+            attrs={
+                "class": "bg-blanco border-2 border-verder_claro focus:border-verder_claro focus:ring-verder_claro",
+            },
+        ),
+    )
+
+    class Meta:
+        model = User
+        fields = ("username", "password", "checkbox")
+        
--- register/views.py
+++ register/views.py
@@ -1,47 +1,44 @@
 from django.shortcuts import render,redirect
+from django.urls import reverse
+from django.http import JsonResponse
 from django.contrib import messages
 from django.contrib.auth import login, logout,authenticate
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import  login_required
-
+from . import forms
 
 # Create your views here.
 def signup_view(request):
-    #Si el usuario ya esta autenticado, lo redirige a la pagina principal
     if request.user.is_authenticated:
         return redirect('main:home')
-    #Si el metodo es POST, valida los datos del formulario y crea el usuario
+    
     if request.method == 'POST':
-        #Obtiene los datos del formulario
         username = request.POST['username']
         password1 = request.POST['password1']
         password2 = request.POST['password2']
-        terminos = request.POST.get('terminos')
-        
-        #Valida los datos del formulario y muestra mensajes de error si es necesario
+        checkbox = request.POST.get('checkbox')
+
         if password1 != password2:
-            messages.error(request, "Las contraseñas no coinciden.")
-            return render(request, 'register/pages/signup.html')
+            return JsonResponse({"error": "Las contraseñas no coinciden."})
         elif len(password1) < 8:
-            messages.error(request, "La contraseña debe tener al menos 8 caracteres.")
-            return render(request, 'register/pages/signup.html')
+            return JsonResponse({"error": "La contraseña debe tener al menos 8 caracteres."})
         elif User.objects.filter(username=username).exists():
-            messages.error(request, "El nombre de usuario ya está en uso.")
-            return render(request, 'register/pages/signup.html')
-        elif terminos != 'on':
-            messages.error(request, "Debes aceptar los términos y condiciones.")
-            return render(request, 'register/pages/signup.html')
-        
-        #Crea el usuario usando el modelo User de Django y automaticamente lo loguea, 
-        # para despues redirigirlo a la pagina principal
+            return JsonResponse({"error": "El nombre de usuario ya está en uso."})
+        elif checkbox != 'on':
+            return JsonResponse({"error": "Debes aceptar los términos y condiciones."})
+
         user = User.objects.create_user(username=username, password=password1)
         login(request, user)
-        messages.success(request, "Usuario creado exitosamente.")
-        return redirect('main:home')
-    #Si el metodo es GET, muestra la pagina de registro
-    return render(request, 'register/pages/signup.html')
+        
+        return JsonResponse({"success": "Usuario creado exitosamente.", "redirect": reverse('main:home')})
+
+    form = forms.SignUpForm()
+    # Si el método es GET o cualquier otro, muestra la página de registro
+    return render(request, 'register/pages/signup.html', {"form": form})
+
 
 def login_view(request):
+    
     #Si el usuario ya esta autenticado, lo redirige a la pagina principal
     if request.user.is_authenticated:
         return redirect('main:home')
@@ -55,20 +52,18 @@ def login_view(request):
         #Obtiene datos del usuario y valida si existe 
         user = User.objects.filter(username=username).first()
         if user is None:
-            messages.error(request, "El usuario no existe.")
-            return render(request, 'register/pages/login.html')
+            return JsonResponse({"error": "El usuario no existe."})
         # Valida la contraseña del usuario
         user = authenticate(request, username=username, password=password)
         if user is None:
-            messages.error(request, "La contraseña es incorrecta.")
-            return render(request, 'register/pages/login.html')
+            return JsonResponse({"error": "La contraseña es incorrecta."})
         #Loguea al usuario y lo redirige a la pagina principal
         login(request, user)
-        messages.success(request, "Sesión iniciada exitosamente.")
-        return redirect('main:home')
+        return JsonResponse({"success": "Sesión iniciada exitosamente.", "redirect": reverse('main:home')})
     
+    form = forms.LoginForm()
     #Si el metodo es GET, muestra la pagina de login
-    return render(request, 'register/pages/login.html')
+    return render(request, 'register/pages/login.html', {"form": form})
 
 @login_required
 def logout_view(request):
