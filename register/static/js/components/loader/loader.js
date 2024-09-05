786118c16183903f3195b7c237c1479e86d473cf
lesquel <holahi23holahi23@gmail.com>
2024-09-05 17:13:39 -0500

    css

--- register/static/js/components/loader/loader.js
+++ register/static/js/components/loader/loader.js
@@ -1,6 +1,17 @@
 export const createLoader = () => {
     const loader = document.createElement('div');
     loader.classList.add("loader-div-container");
+    loader.setAttribute('style', `position: fixed;
+    top: 0px;
+    left: 0px;
+    display: flex;
+    height: 100vh;
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+    --tw-backdrop-blur: blur(4px);
+    -webkit-backdrop-filter: var(--tw-backdrop-blur) var(--tw-backdrop-brightness) var(--tw-backdrop-contrast) var(--tw-backdrop-grayscale) var(--tw-backdrop-hue-rotate) var(--tw-backdrop-invert) var(--tw-backdrop-opacity) var(--tw-backdrop-saturate) var(--tw-backdrop-sepia);
+    backdrop-filter: var(--tw-backdrop-blur) var(--tw-backdrop-brightness) var(--tw-backdrop-contrast) var(--tw-backdrop-grayscale) var(--tw-backdrop-hue-rotate) var(--tw-backdrop-invert) var(--tw-backdrop-opacity) var(--tw-backdrop-saturate) var(--tw-backdrop-sepia);`);
     loader.innerHTML = '<div class="loader"></div>';
     document.body.appendChild(loader);
     return loader;
